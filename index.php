3e58a09a556dc678949fffa724b7ec6107b7a864
Rafael Correia Paz <rafael.cp@outlook.com>
2017-06-21 23:02:28 -0300

    Esqueleto do layout

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>iFleet - Sistema de Controle de Frotas</title>
+		<link rel="stylesheet" type="text/css" href="semantic/semantic.min.css">
+	</head>
+	<body>
+		
+		<div class="ui grid container">
+			<div class="ui vertical menu four wide column">
+				<div class="item">
+					<div class="header">Products</div>
+					<div class="menu">
+						<a class="item">Enterprise</a>
+						<a class="item">Consumer</a>
+					</div>
+				</div>
+				<div class="item">
+					<div class="header">CMS Solutions</div>
+					<div class="menu">
+						<a class="item">Rails</a>
+						<a class="item">Python</a>
+						<a class="item">PHP</a>
+					</div>
+				</div>
+				<div class="item">
+					<div class="header">Hosting</div>
+					<div class="menu">
+						<a class="item">Shared</a>
+						<a class="item">Dedicated</a>
+					</div>
+				</div>
+				<div class="item">
+					<div class="header">Support</div>
+					<div class="menu">
+						<a class="item">E-mail Support</a>
+						<a class="item">FAQs</a>
+					</div>
+				</div>
+			</div>
+			<div class="eight wide column">
+				a
+			</div>
+		</div>
+		<script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+		<script src="semantic/semantic.min.js"></script>
+	</body>
+</html>
